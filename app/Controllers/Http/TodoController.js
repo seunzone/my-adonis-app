7fad595e374637aba8064f71826c8c9c6299f9d3
seunzone <darealseun@gmail.com>
2019-03-30 16:15:40 +0100

    validate user input using adonis and display error message to the user

--- app/Controllers/Http/TodoController.js
+++ app/Controllers/Http/TodoController.js
@@ -1,14 +1,27 @@
 'use strict'
+const { validateAll } = use('Validator');
 const Todo = use('App/Models/Todo');
 
 class TodoController {
     // async index ({ view }) {
     //     const todos = await TodoController.call();
     // }
-    async store ({ request, response }) {
+    async store ({ request, response, session }) {
         const body = request.all();
-        // console.log(request.all())
-        await Todo.create({
+        const rules = {
+            text: 'required|min:8'
+        }
+        const messages = {
+            'text.required': 'Todo list cannot be empty',
+            'text.min': 'Todo must be at least 5 characters long'
+        }
+        const validator = await validateAll(body, rules, messages);
+        if (validator.fails()){
+        session.withErrors(validator.messages()).flashAll();
+        return response.redirect('/');
+            
+        }
+        const todo = await Todo.create({
             text: body.text
         })
         // console.log(todo)
